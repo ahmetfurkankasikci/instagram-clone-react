6ad282762e1a2c436c6d00cb4976b8b8903d35bd
Ahmet Furkan Kaşıkçı <56443438+kashichi@users.noreply.github.com>
2021-07-27 20:13:00 +0300

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@
 import firebase from 'firebase';
 
 const firebaseApp =firebase.initializeApp( {
-    apiKey: "AIzaSyBTmcY2H7rtS2VGzEk2WJSEce12QlNrYbc",
-    authDomain: "instagram-clone-react-66914.firebaseapp.com",
-    projectId: "instagram-clone-react-66914",
-    storageBucket: "instagram-clone-react-66914.appspot.com",
-    messagingSenderId: "924766152842",
-    appId: "1:924766152842:web:d9fa979e9c2e47bcd82573",
-    measurementId: "G-TCRSYRQ0TB"
+ //firebase settings
   });
   const db=firebaseApp.firestore();
   const auth=firebase.auth();
